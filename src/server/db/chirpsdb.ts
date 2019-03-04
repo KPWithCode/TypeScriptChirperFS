5d1f5766cbfa3e8844ae35c5b5094442d98ac938
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2019-03-04 17:26:42 -0500

    Added the appropriate database calls to update delete chirps

--- src/server/db/chirpsdb.ts
+++ src/server/db/chirpsdb.ts
@@ -5,16 +5,28 @@ const all = async () => Query('SELECT * FROM Chirps')
 
 const oneChirp = async (id: number) =>
   Query(
-      'SELECT * FROM Chirps WHERE id = ?',[id]
+    'SELECT * FROM Chirps WHERE id = ?', [id]
     // "select u.name, c.text as chirp from users u join chirps c on u.id =  c.userid where c.id = ?;",
     // [id]
   );
 
-  const post = async (id: number, text: string, name: string) =>
-  Query("CALL sp_checking(?,?,?)", [text, name, id]);
+const post = async (id: number, text: string, name: string) =>
+  Query(
+    "CALL sp_checking(?,?,?)", [text, name, id]
+  );
+
+const remove = async (id: number) => {
+  Query("CALL sp_deleting(?)", [id]);
+};
+
+const update = async (text: string, name: string, id: number) => {
+  Query('CALL sp_updating(?,?,?)', [id, text, name]);
+};
 
 export default {
-    all,
-    oneChirp,
-    post
+  all,
+  oneChirp,
+  post,
+  remove,
+  update
 }
