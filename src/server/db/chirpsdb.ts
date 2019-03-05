24254194ab7a4696291e5bdd94dd9aaa8181eb4f
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2019-03-05 01:42:24 -0500

    added correct Query for post

--- src/server/db/chirpsdb.ts
+++ src/server/db/chirpsdb.ts
@@ -6,21 +6,18 @@ const all = async () => Query('SELECT * FROM Chirps')
 const oneChirp = async (id: number) =>
   Query(
     'SELECT * FROM Chirps WHERE id = ?', [id]
-    // "select u.name, c.text as chirp from users u join chirps c on u.id =  c.userid where c.id = ?;",
-    // [id]
   );
 
-const post = async (id: number, text: string, name: string) =>
-  Query(
-    "CALL sp_checking(?,?,?)", [text, name, id]
-  );
+const post = async (userid: number, chirptext: string) =>
+  Query(`INSERT INTO Chirps (userid, chirp) VALUES (${userid}, ${chirptext}`);
+  
 
 const remove = async (id: number) => {
-  Query("CALL sp_deleting(?)", [id]);
+  Query('DELETE FROM Chirps WHERE id = ?', [id]);
 };
 
 const update = async (text: string, name: string, id: number) => {
-  Query('CALL sp_updating(?,?,?)', [id, text, name]);
+  Query('CALL sp_updating(?,?,?)', [id, text, name])
 };
 
 export default {
