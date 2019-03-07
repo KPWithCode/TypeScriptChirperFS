43ac66db522724de65e839f9f8a166771c092e02
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2019-03-07 14:42:58 -0500

    Cleaned up unnecessary comments

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -20,27 +20,6 @@ class App extends React.Component<IAppProps, IAppState> {
         )
     }
 }
-// constructor(props: IAppProps) {
-//     super(props);
-
-//     this.state = { name: null };
-// }
-
-// async componentWillMount() {
-//     let r = await fetch('/api/hello');
-//     let name = await r.json();
-//     this.setState({ name })
-// }
-
-// render () {
-//     return (
-//         <main className="container">
-//             <h1 className="covalence-blue">Hello {this.state.name}!</h1>
-//             <h2></h2>
-//         </main>
-//     )
-// }
-
 
 interface IAppProps {
 
